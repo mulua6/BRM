4af9569643e28ee2b755b58477c66097585c6041
mulua6 <mulua6@gmail.com>
2015-12-19 15:22:28 +0800

    commit java

--- src/main/java/com/mio/demo/TestDemo1.java
+++ src/main/java/com/mio/demo/TestDemo1.java
@@ -4,4 +4,9 @@ package com.mio.demo;
  * Created by liuhe on 15/12/19.
  */
 public class TestDemo1 {
+
+    public void test1(){
+
+    }
+
 }
---
+++ src/test/java/com/mio/testDemo/Demo1Test.java
@@ -0,0 +1,8 @@
+package com.mio.testDemo;
+
+/**
+ * Created by liuhe on 15/12/19.
+ */
+public class Demo1Test {
+
+}
